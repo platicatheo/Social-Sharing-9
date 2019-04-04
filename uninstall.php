1d54bf316d86d0247c90894975be284314b00f1c
platicatheo <platicatheo@gmail.com>
2019-04-05 01:24:17 +0300

    Second WP review

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,11 @@
 <?php
 
+// if uninstall.php is not called by WordPress, die
+if( !defined('WP_UNINSTALL_PLUGIN') ) {
+	die;
+}
+
+// drop social_sharing_9 database table
 global $wpdb;
 $table_name =  $wpdb->prefix."social_sharing_9";
 $sql = "DROP TABLE IF EXISTS $table_name;";
